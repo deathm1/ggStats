97a5420bc19042b427db31e3f076e7fdee38c142
deathm1 <k0shur98@outlook.com>
2022-09-15 14:35:47 +0530

    final v1.0.1, minor changes

--- server/config/configurator.js
+++ server/config/configurator.js
@@ -1,19 +1,19 @@
-const fs = require("fs");
-const path = require("path");
-const ini = require("ini");
-const logger = require("./logger");
+const fs = require('fs')
+const path = require('path')
+const ini = require('ini')
+
 const config = ini.parse(
-  fs.readFileSync(path.join(__dirname, "./config.ini"), "utf-8")
-);
+  fs.readFileSync(path.join(__dirname, './config.ini'), 'utf-8'),
+)
 
 if (parseInt(process.env.SERVER_STORAGE) == 1) {
-  const logger = require("./logger");
+  const logger = require('./logger')
   module.exports = function () {
-    return { config, logger };
-  };
+    return { config, logger }
+  }
 } else {
   module.exports = function () {
-    const logger = undefined;
-    return { config, logger };
-  };
+    const logger = undefined
+    return { config, logger }
+  }
 }
